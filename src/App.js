8597ac8d0f0e2b3d561275d8fa7cc5844bd899b7
vaedym <krasnoshchokvadim@gmail.com>
2021-08-06 19:48:40 +0300

    first blank layout with router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
 
-function App() {
+// importing components
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Test from "./components/Test/Test";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <div className="contentWrapper">
+          <Route exact path="/" component={Main} />
+          <Route path="/test" component={Test} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
